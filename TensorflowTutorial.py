474100e11105bacc2343a8318e976007de35d801
JunJin1218 <jin_jun@mymail.sutd.edu.sg>
2024-05-23 17:05:43 +0800

    TensorFlow Keras Tutorial 2

--- TensorflowTutorial.py
+++ TensorflowTutorial.py
@@ -4,3 +4,13 @@ import numpy as np
 X_train_data = np.array([1, 2, 3, 4, 5])
 Y_train_data = np.array([4, 7, 10, 13, 16])
 
+#The best place to start is with the user-friendly Keras sequential API
+
+"""
+1. Load a prebuilt dataset.
+2. Build a neural network machine learning model that classifies images.
+3. Train this neural network.
+4. Evaluate the accuracy of the model
+"""
+
+print("")
